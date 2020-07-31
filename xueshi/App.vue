2289af327b0c932db4e69157834efa2850d3a22f
wangsdong2008 <wangsdong2008@163.com>
2020-07-31 19:27:20 +0800

    头部插件

--- xueshi/App.vue
+++ xueshi/App.vue
@@ -12,6 +12,146 @@
 	}
 </script>
 
-<style>
+<style lang="scss">	
 	/*每个页面公共css */
+	.main_content{
+		width: 100%;
+	}
+	
+	.grey{
+		color:$color;
+	}
+	.content{
+		width: $content_width;
+		margin: 0 auto;
+		margin-top: 40upx;
+	}
+	.content .title{
+		border-bottom: 1px solid $bgcolor;
+		height: 45upx;
+		line-height: 45upx;
+		margin: 30upx 0upx;
+		padding-bottom: 30upx;
+		background-color: #fff;
+	}
+	
+	.title image{
+		width: 50upx;
+		height: 50upx;
+		margin-right: 20upx;
+	}
+	.content ul{
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+	}
+	
+	navigator{
+		display: inline;
+		color:#0000ff;
+	}
+	.clear{
+		clear: both;
+	}
+	
+	.btn-row{
+		margin-top: 40upx;
+	}
+	
+	.btn{
+		width: 100%;
+		text-align: center;
+		background-color: #fff;
+		font-size: 35upx;
+		border-radius: 50upx;
+		margin-bottom: 30upx;
+	}
+	
+	.footer{
+		height: 100upx;
+	}
+	
+	input.register-input{
+		height: 100upx;
+		line-height: 100upx;
+		border-bottom: 1upx solid #ccc;
+		text-indent: 70upx;
+	}
+	input.register-input-mobile{
+		background:url(@/static/img/mobile.png) 10upx 22upx no-repeat;
+		-webkit-background-size:40upx 50upx ;
+		background-size:40upx 50upx ;		
+	}
+	input.register-input-password{
+		background:url(@/static/img/password.png) 10upx 22upx no-repeat;	
+		-webkit-background-size:40upx 50upx ;
+		background-size:40upx 50upx ;
+	}
+	input.register-input-mail{
+		background:url(@/static/img/mail.png) 10upx 22upx no-repeat;	
+		-webkit-background-size:40upx 52upx ;
+		background-size:40upx 52upx ;
+	}
+	
+	//头像
+	.boys{
+		background:url(@/static/img/boy.png) 10upx 20upx no-repeat;
+		-webkit-background-size:50upx 50upx ;
+		background-size:50upx 50upx ;
+	}
+	.grils{
+		background:url(@/static/img/gril.png) 10upx 20upx no-repeat;
+		-webkit-background-size:50upx 50upx ;
+		background-size:50upx 50upx ;
+	}
+	
+	
+	/* 头部信息 */
+	.header-title{
+		background:url('@/static/img/login_title.png') #ffffff center 0 no-repeat;
+	    background-size:100% 100%;
+	    padding-bottom:20%
+	}
+	.login_center{
+		width:85%;			
+		margin: 0 auto;
+	}
+	.login_title_txt{
+	    color:#fff;
+	    font-family:'微软雅黑';
+	    font-size:60upx;
+	    padding-top:150upx;
+	}	
+	.login_title_txt span{
+	    font-size: 48upx;
+	}	
+	/*头部css*/
+	.fz20{
+		font-size: 20upx;
+	}
+	.fz30{
+		font-size: 30upx;
+	}
+	.fz35{
+		font-size: 35upx;
+	}
+	.fz40{
+		font-size: 40upx;
+	}
+	
+	//验证码按纽
+	.yzm{		
+		text-align: center;
+		border-radius: 50upx;
+		float: right;
+		width:40%;
+		border: 0upx;
+		color:#fff;
+		background-color: $bgcolor;
+		line-height: 60upx;
+		height: 60upx;
+	}
+	.yzm::after{
+		border: 0upx;
+	}
 </style>
---
+++ xueshi/components/header/company_header.vue
@@ -0,0 +1,77 @@
+<template>
+	<view class="header-title">
+	    <view class="login_center login_title_txt">{{name}}<br /><span>{{en_name}}</span></view>
+		<view class="logo" :style="'background:url('+logo+') 0upx 0upx no-repeat;-webkit-background-size: 320upx 320upx;background-size: 320upx 320upx;'"></view>
+	</view>	
+</template>
+<style>
+	.header-title{
+		position: relative;
+	}
+	.login_title_txt{
+		z-index: 100;
+		position: relative;
+	}
+	.logo{
+		width:320upx;
+		height: 320upx;
+		position:absolute;
+		top:90upx;
+		left:400upx;
+		/* animation:mymove 2s infinite;
+		-webkit-animation:mymove 2s infinite; */ /*Safari and Chrome*/
+		animation-iteration-count:1;
+		-webkit-animation-iteration-count:1;
+		
+	}
+	@keyframes mymove
+	{
+		from {left:0upx;}
+		to {left:400upx;}
+	}
+	
+	@-webkit-keyframes mymove /*Safari and Chrome*/
+	{
+		from {left:0upx;}
+		to {left:400upx;}
+	}	
+		
+	
+</style>
+
+<script>
+	export default {
+		props:{
+			msg: String
+		},
+		data() {
+			return {
+				msgarr:'',
+				name:'',
+				en_name:''
+			}
+		},
+		created:function(){
+			this.msgarr = this.msg;
+			uni.hideTabBar({});	
+			this.init();
+			//console.log(this.msg);
+		},	
+		methods:{
+			init(){
+				let arr = this.msgarr.split(",");
+				this.name = arr[0];
+				this.en_name = arr[1];
+				//debugger;
+				/* 
+				let ret = this.getUserInfo();
+				if(!ret){					
+					return false;
+				}
+				else{
+					this.logo = ret.logo;
+				} */
+			}
+		}
+	}
+</script>
